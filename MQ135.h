45725ffb8d777afba7875aaf3a54315d8480053d
EliasMahmoudi11 <45590866+EliasMahmoudi11@users.noreply.github.com>
2018-12-04 14:04:55 +0000

    Update MQ135.h

--- MQ135.h
+++ MQ135.h
@@ -1,18 +1,4 @@
-/**************************************************************************/
-/*!
-@file     MQ135.h
-@author   G.Krocker (Mad Frog Labs)
-@license  GNU GPLv3
 
-First version of an Arduino Library for the MQ135 gas sensor
-TODO: Review the correction factor calculation. This currently relies on
-the datasheet but the information there seems to be wrong.
-
-@section  HISTORY
-
-v1.0 - First release
-*/
-/**************************************************************************/
 #ifndef MQ135_H
 #define MQ135_H
 #if ARDUINO >= 100
@@ -21,11 +7,12 @@ v1.0 - First release
  #include "WProgram.h"
 #endif
 
-/// The load resistance on the board
-#define RLOAD 10.0
-/// Calibration resistance at atmospheric CO2 level
-#define RZERO 76.63
-/// Parameters for calculating ppm of CO2 from sensor resistance
+#define RLOAD 10.0 //potentiometer
+
+#define RZERO 70.61 // Voltage value
+
+/// Parameters to model temperature and humidi
+
 #define PARA 116.6020682
 #define PARB 2.769034857
 
@@ -36,7 +23,7 @@ v1.0 - First release
 #define CORD 0.0018
 
 /// Atmospheric CO2 level for calibration purposes
-#define ATMOCO2 397.13
+#define ATMOCO2 494.63
 
 class MQ135 {
  private:
